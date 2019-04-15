ef43445ad410eb355dc137b8c4febd14faaed4c9
pedrodonascimento <pedrodonascimento@tjrj.jus.br>
2019-04-15 10:18:25 -0300

    Inclusão do Back-end

---
+++ backend-infobase/src/main/java/com/br/prova/infobase/backendinfobase/BackendInfobaseApplication.java
@@ -0,0 +1,13 @@
+package com.br.prova.infobase.backendinfobase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendInfobaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendInfobaseApplication.class, args);
+	}
+
+}
